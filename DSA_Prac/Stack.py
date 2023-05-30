ac088c73c7c940e7ac9afd6f9efb1d365e175cba
Ramanjeet Saini <rj.s.saini@gmail.com>
2023-05-30 17:02:06 -0500

    Stack learning

---
+++ DSA_Prac/Stack.py
@@ -0,0 +1,35 @@
+class Stack():
+    def __init__(self):
+        self.items=[]
+
+    def push(self,item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def get_stack(self):
+        return self.items
+    
+    def is_empty(self):
+        return self.items==[]
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return "Empty Stack"
+
+    
+myStack = Stack()
+myStack.push('A')
+myStack.push('B')
+print(myStack.get_stack())
+myStack.push('C')
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
+print(myStack.is_empty())
+myStack.pop()
+myStack.pop()
+print(myStack.peek())
